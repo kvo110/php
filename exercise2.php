c9fc11f9cc99cf72fb0a1d710b64aa2a01c59976
Kenny Vo <voghuy17@gmail.com>
2026-03-05 14:45:53 -0500

    Add calculateArea() and validate numeric input before calculating

--- exercise2.php
+++ exercise2.php
@@ -1,8 +1,30 @@
 <?php
   /*
     exercise2.php
-    Mission 2 placeholder
+    Mission 2: Geometric Area Calculator
+
+    Student note:
+    - calculateArea() returns a number.
+    - Input is checked with is_numeric() before using it.
   */
+
+  function calculateArea($length, $width) {
+    return $length * $width;
+  }
+
+  $length = 7.4;
+  $width = 7.5;
+
+  $message = "";
+
+  if (!is_numeric($length) || !is_numeric($width)) {
+    $message = "Please enter numeric values for length and width.";
+  } elseif ($length < 0 || $width < 0) {
+    $message = "Length and width must be 0 or greater.";
+  } else {
+    $area = calculateArea((float)$length, (float)$width);
+    $message = "The area of the room is: " . $area . " sq ft";
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +35,9 @@
 </head>
 <body>
   <h1>Mission 2: Geometric Area Calculator</h1>
-  <p>Work in progress.</p>
+
+  <?php echo htmlspecialchars($message); ?>
+
   <p><a href="index.php">Back to index</a></p>
 </body>
 </html>
